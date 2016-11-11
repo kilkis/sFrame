ecb35b13d4838ccadb03e8966bf3222d8872640a
kilkis <kilkis@163.com>
2016-11-11 18:17:42 +0800

    add monster editor for export to server
    1.add editor monster prefab

---
+++ sClient/Assets/sFramework/ServerInfoExport/sEditor_Monster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sEditor_Monster : MonoBehaviour {
+
+	public int monsterID;
+	public float viewRange = 5.0f;
+	public float backRange = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawLine (transform.position, transform.position + 2.0f * transform.forward);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere (transform.position, viewRange);
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere (transform.position, backRange);
+	}
+}
