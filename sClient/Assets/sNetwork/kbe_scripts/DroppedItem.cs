0214bf0f654f921f7dd52b0c250a80a0e0c539f3
kilkis <kilkis@163.com>
2016-12-19 19:47:21 +0800

    增加物品和背包相关内容（客户端）
    修正服务器配置错误

---
+++ sClient/Assets/sNetwork/kbe_scripts/DroppedItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KBEngine
+{
+    public class DroppedItem : KBEngine.GameObject
+    {
+        public override void __init__()
+        {
+        }
+
+        public void pickUpRequest()
+        {
+            cellCall("pickUpRequest");
+        }
+    }
+}
